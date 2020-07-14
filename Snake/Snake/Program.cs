ae3b38fb7d85b49fa995a2a61fc2f7e98e929a34
hichzz <grimmjow49@gmail.com>
2020-07-14 13:37:34 +0300

    Lesson-5. RAM and test

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,7 +12,51 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Console.ReadLine();
+            int x = 1;
+            Func1(x);
+            Console.WriteLine($"Func1 x={x}");
+
+            Func2(x);
+            Console.WriteLine($"Func2 x={x}");
+
+            Func3(x);
+            Console.WriteLine($"Func3 x={x}");
+
+            Move(p1, 10, 10);
+            Console.WriteLine($"Move(p1, 10, 10) p1.x = {p1.X}, p1.y = {p1.Y}");
+
+            Reset(p2, 10, 10);
+            Console.WriteLine($"Reset(p2) p2.x = {p2.X}, p2.y = {p2.Y}");
+
+            Console.ReadKey();
+        }
+
+        private static void Reset(Point p, int dx, int dy)
+        {
+            p = new Point();
+            p.X = p.X + dx;
+            p.Y = p.Y + dy;
+        }
+
+        private static void Move(Point p, int dx, int dy)
+        {
+            p.X = p.X + dx;
+            p.Y = p.Y + dy;
+        }
+
+        private static void Func3(int x)
+        {
+            x++;
+        }
+
+        private static void Func2(int val)
+        {
+            val++;
+        }
+
+        private static void Func1(int x)
+        {
+
         }
 
     }
