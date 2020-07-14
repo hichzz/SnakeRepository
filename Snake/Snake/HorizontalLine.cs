7fb04de2188888e3928d61dc81cd72a7a10ea6f7
hichzz <grimmjow49@gmail.com>
2020-07-14 14:23:29 +0300

    Lesson 7. Figure. Inheritance

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -4,10 +4,8 @@ using System.Text;
 
 namespace Snake
 {
-    class HorizontalLine
+    class HorizontalLine : Figure
     {
-        private List<Point> points;
-
         public HorizontalLine(int xLeft, int xRight, int y, char symbol)
         {
             points = new List<Point>();
@@ -15,14 +13,6 @@ namespace Snake
             {
                 Point p = new Point(x, y, symbol);
                 points.Add(p);
-            }
-        }
-
-        public void Draw()
-        {
-            foreach (Point p in points)
-            {
-                p.Draw();
             }
         }
     }
--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -4,10 +4,8 @@ using System.Text;
 
 namespace Snake
 {
-    class VerticalLine
+    class VerticalLine : Figure
     {
-        private List<Point> points;
-
         public VerticalLine(int x, int yUp, int yDown, char symbol)
         {
             points = new List<Point>();
@@ -15,14 +13,6 @@ namespace Snake
             {
                 Point p = new Point(x, y, symbol);
                 points.Add(p);
-            }
-        }
-
-        public void Draw()
-        {
-            foreach (Point p in points)
-            {
-                p.Draw();
             }
         }
     }
