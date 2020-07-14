a09131583bcd6760ce4d37062a5332ca77adc23d
hichzz <grimmjow49@gmail.com>
2020-07-14 13:10:01 +0300

    Lesson 2. Draw function.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,7 +6,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1;
+            int y1 = 3;
+
+            Draw(x1, y1, '*');
+
+            Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char symbol)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(symbol);
         }
     }
 }
